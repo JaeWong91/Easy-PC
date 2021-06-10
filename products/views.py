8ce8afb2376e60ec157ce26f6132f7f7482c6148
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-10 17:00:55 +0000

    Added products views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):   # video tutorial here is "all_products"
+    """ A view to show the graphics cards, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
