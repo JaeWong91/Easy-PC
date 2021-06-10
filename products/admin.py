99fe6faae5cd21e9a32f2280396d04f24df94575
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-10 20:01:30 +0000

    added product details functionality and some products into database

--- products/admin.py
+++ products/admin.py
@@ -7,6 +7,7 @@ from .models import Product, Category
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
+        'pk',   # added this myself because not auto generated???
         'name',
         'category',
         'price',
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 
-def all_products(request):   # video tutorial here is "all_products"
-    """ A view to show the graphics cards, including sorting and search queries """
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
 
@@ -14,3 +14,15 @@ def all_products(request):   # video tutorial here is "all_products"
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product detail """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
