731d25ab0938ace3dee63fefe325a153c19fdcaf
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-12 21:46:02 +0000

    added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -24,3 +24,31 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag    # and then overwrite the variable in the session with the updated version.
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ adjust the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))    # converting to integer since itll come from the template as a string
+    bag = request.session.get('bag', {})    # this bag variable will access the request's session, trying to get the variable if it exists or initializing it to an empty dictionary if it doesn't
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag    # and then overwrite the variable in the session with the updated version.
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ remove the item from shopping bag """
+    try:
+        bag = request.session.get('bag', {})    # this bag variable will access the request's session, trying to get the variable if it exists or initializing it to an empty dictionary if it doesn't
+        bag.pop(item_id)
+
+        request.session['bag'] = bag    # and then overwrite the variable in the session with the updated version.
+        return HttpResponse(status=200)     # use HttpResponse 200 status to imply item was successfully removed
+
+    except Exception as e:
+        return HttpResponse(status=500)
