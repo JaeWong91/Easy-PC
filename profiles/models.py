bb448d5314afd43a8260e26a4f676d7717769286
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-18 17:04:16 +0000

    Updated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -11,28 +11,23 @@ class UserProfile(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)         # This is like a foreignkey except it specifies that each user can only have one profile, and each profile can only be attached to one user.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)       # this not required
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)         # this not required
+    default_county = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
 
 
-""" 
-quick receiver for the post save event from the user model so that each time a user object is saved.
-we'll automatically either create a profile for them if the user has just been created,
-or just save the profile to update if the user already existed
-"""
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    create or update the user profile
+    Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
