eb20e25e9a712d97f1aea5db256fd3b20b2b3235
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-18 20:40:50 +0000

    added order history and updated checkout views

--- checkout/models.py
+++ checkout/models.py
@@ -10,7 +10,6 @@ from products.models import Product     # this is needed because of the foreign 
 from profiles.models import UserProfile
 
 
-
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)  # automatic generate so its unique
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
