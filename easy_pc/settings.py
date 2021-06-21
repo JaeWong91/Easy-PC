bd407ae567b93477addfe15a05ae4327285fd662
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-21 13:47:04 +0000

    secret key

--- easy_pc/settings.py
+++ easy_pc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'grtrqk8dm%6n0_njm6gx990in^-98kdvf&=*70q7yut2w&4%gl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
