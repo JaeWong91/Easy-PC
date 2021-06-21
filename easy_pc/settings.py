e01636e509e82be3ffd6d749b7756e20e9e65410
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-21 14:04:25 +0000

    Removed secret key and set debug

--- easy_pc/settings.py
+++ easy_pc/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'grtrqk8dm%6n0_njm6gx990in^-98kdvf&=*70q7yut2w&4%gl'
+SECRET_KEY = os.get.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['easy-pc-jw.herokuapp.com', 'localhost']
 
@@ -123,13 +123,13 @@ WSGI_APPLICATION = 'easy_pc.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default' :dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
