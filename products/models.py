c3e6a79919b308ed613935eaf325ab43e5fdebd2
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-22 11:32:45 +0000

    enable deletion of reviews in admin

--- products/models.py
+++ products/models.py
@@ -48,11 +48,10 @@ class Product(models.Model):
 class ProductReview(models.Model):
     product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
-
     content = models.TextField(blank=True, null=True)
     rating = models.IntegerField()
 
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.rating
+        return self.content
