8019bfc26145dd9bb73b1f425441a3e2ef99c72b
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-22 19:39:52 +0000

    create article_details template, view and url

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import BlogView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.blog, name='blog'),
+    #path('', views.blog, name='blog'),
+    path('', BlogView.as_view(), name='blog'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
-def blog(request):
-    return render(request, 'blog/blog.html', {})
+# def blog(request):
+#     return render(request, 'blog/blog.html', {})
+
+class BlogView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'blog/article_details.html'
