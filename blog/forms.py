70d4ebf7fbf743e8b9a0138a76c7534b14ce5198
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-23 17:08:51 +0000

    removed author field from blog model

--- blog/forms.py
+++ blog/forms.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'body')
+        fields = ('title', 'body')
 
         widgets = {
             # bootstrap 'form-control' class
             'title': forms.TextInput(attrs={'class': 'border-purple rounded-0 mb-2'}),
             # 'author': User,     try to make it so that the current sesion user is the author!!
-            'author': forms.Select(attrs={'class': 'border-purple rounded-0 mb-2'}),
+            # 'author': forms.Select(attrs={'class': 'border-purple rounded-0 mb-2'}),
             'body': forms.Textarea(attrs={'class': 'border-purple rounded-0 mb-2'}),
         }
 
--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,12 @@ from datetime import datetime, date
 class Post(models.Model):
     title = models.CharField(max_length=255)
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    # author = models.OneToOneField(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.title + ' | ' + str(self.author)
+        return self.title           # + ' | ' + str(self.author)
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.pk)])
