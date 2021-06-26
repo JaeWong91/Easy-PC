18b662a5817a1c0c6f69f22f7adc17159729b2a9
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-26 17:25:16 +0000

    exclude 'rating' field in add product form

--- products/forms.py
+++ products/forms.py
@@ -7,7 +7,8 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ('rating',)
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
     length = forms.DecimalField(label='Length (centimetres)')
