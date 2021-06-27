b02e6e74e87b239a3ab73b1b56f20b756dd48dd6
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-27 16:51:23 +0000

    tidy up code

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,31 +1,26 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product     # importing Product class from the products.models
+from products.models import Product
 
-# This is a called a "context processor". Its purpose is to make this
-# dictionary available to all templates across the entire application
+
 def bag_contents(request):
 
-    bag_items = []  # start with empty list
-    total = 0   # initialize the total to zero
-    product_count = 0   # initialize the product count to zero
-    bag = request.session.get('bag', {})     # getting bag if it already exists or initializing a dictionaty if not
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
 
-    # this is a loop to show the total price of the shopping bag on the navbar
-    # bag.items() is the bag from the session
-    for item_id, item_data in bag.items():  # in the below lines, we changed "item_date" from "quantity". the video lessons did this for the sizes of clothing because items may have same id but different sizes
+    for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         total += item_data * product.price
         product_count += item_data
         bag_items.append({
             'item_id': item_id,
             'quantity': item_data,
-            'product': product,     # adding the product object itself so can access the images and so on
+            'product': product,
         })
 
-    # this to calculate if qualifies for free delievery.
-    # The STANDARD_DELIVERY_PERCENTAGE is 10% from settings.py at the bottom
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
@@ -35,8 +30,6 @@ def bag_contents(request):
 
     grand_total = delivery + total
 
-    # this is to add the below to the context so they'll be available
-    # throughout the whole site
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -15,9 +15,9 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))    # converting to integer since itll come from the template as a string
-    redirect_url = request.POST.get('redirect_url')     # this is to get the redirect url from the form 
-    bag = request.session.get('bag', {})    # this bag variable will access the request's session, trying to get the variable if it exists or initializing it to an empty dictionary if it doesn't
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
 
     # this will update the bag or update the quantity if it already exists
     if item_id in list(bag.keys()):
@@ -27,16 +27,16 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
 
-    request.session['bag'] = bag    # and then overwrite the variable in the session with the updated version.
+    request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
-    """ adjust the quantity of the specified product to the specified amount """
+    """ adjust the quantity of the specified product to the specified amount"""
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))    # converting to integer since itll come from the template as a string
-    bag = request.session.get('bag', {})    # this bag variable will access the request's session, trying to get the variable if it exists or initializing it to an empty dictionary if it doesn't
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
@@ -45,7 +45,7 @@ def adjust_bag(request, item_id):
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag')
 
-    request.session['bag'] = bag    # and then overwrite the variable in the session with the updated version.
+    request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
@@ -54,12 +54,12 @@ def remove_from_bag(request, item_id):
 
     try:
         product = get_object_or_404(Product, pk=item_id)
-        bag = request.session.get('bag', {})    # this bag variable will access the request's session, trying to get the variable if it exists or initializing it to an empty dictionary if it doesn't
+        bag = request.session.get('bag', {})
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag')
 
-        request.session['bag'] = bag    # and then overwrite the variable in the session with the updated version.
-        return HttpResponse(status=200)     # use HttpResponse 200 status to imply item was successfully removed
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
