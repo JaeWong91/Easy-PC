97a7378b71a7781d9e1e7221f9c91988f238ef70
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-06-28 16:06:50 +0000

    removed trailing whitespace

--- checkout/views.py
+++ checkout/views.py
@@ -113,7 +113,7 @@ def checkout(request):
             except UserProfile.DoesNotExist:
                 order_form = OrderForm()
         else:
-            order_form = OrderForm()                
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
